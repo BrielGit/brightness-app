85597c71fcc793e5d73bdadd34b36241d40adeb0
Gabriel Villa <gabrielvillacordero@gmail.com>
2024-08-09 14:24:08 -0300

    finished product

--- brightness.py
+++ brightness.py
@@ -1,19 +1,15 @@
 import screen_brightness_control as sbc
 from tkinter import *
 import tkinter as tk
-from tkinter import ttk
 import threading
 
 brightness = sbc.get_brightness()
-stop_event = threading.Event()
 
-def change_brightness():
-    global scale_int
-    global brightness
-    if brightness != scale_int:
-        master.after(100, sbc.set_brightness(scale_int.get()))
+def change_brightness(value):
+    global idk
+    threading.Thread(target=lambda: sbc.set_brightness(idk.get())).start()
 
-def center_window(master, width, height):
+def center_window(width, height):
     screen_width = master.winfo_screenwidth()
     screen_height = master.winfo_screenheight()
     x1 = width / 2
@@ -24,20 +20,53 @@ def center_window(master, width, height):
     y3 = int(y2 - y1)
     master.geometry(f'{width}x{height}+{x3}+{y3}')
 
-master = tk.Tk()
-center_window(master, 300, 350)
-master.title('Brightness Controller')
+def jump_to_position(event):
+    # Calculate the new slider value based on the click position
+    slider_length = slider.winfo_width()
+    click_position = event.x
+    new_value = (click_position / slider_length) * 100
+    slider.set(new_value)
+    change_brightness(new_value)
 
-# slider
-scale_int = IntVar(value=brightness)
-slider = ttk.Scale(master, 
-                   command = lambda value: change_brightness(), 
-                   from_= 0, 
-                   to = 100,
-                   length = 300,
-                   orient = 'vertical',
-                   variable = scale_int)
+def display_entry_value(event=None):
+    global idk
+    entry_value.delete(0, tk.END)
+    entry_value.insert(0, str(slider.get()))
+
+def entry_useful(event):
+    global idk
+    idk.set(entry_value.get())
+    change_brightness(idk.get())
+
+def validate_entry(value):
+    return value.isdigit() or value == ''
+
+master = tk.Tk()
+center_window(400, 150)
+master.title('Brightness Controller')
+master.configure(bg="#2b2b2b")
+idk = tk.IntVar(value=0)
+
+label_sun = Label(master, text="☀",background='#2b2b2b', fg='white', font=("Arial", 24))
+label_sun.pack(pady=(10, 8), padx=(7, 0))
+
+slider = Scale(master, from_=0, to=100, orient=HORIZONTAL, showvalue=False, variable=idk, command=display_entry_value, length=300, sliderlength=20, bd=0)
+slider.set(brightness)
 slider.pack()
 
+vcmd = (master.register(validate_entry), '%P')
+entry_value = Entry(master, validate='key', validatecommand=vcmd, width=8, justify='center', font=('Courier', 12), bd=4)
+# entry_value.insert(0, brightness)
+entry_value.pack(pady=(15, 0))
 
-tk.mainloop()
+# Bind the brightness change to slider release
+slider.bind("<ButtonRelease-1>", change_brightness)
+
+# Bind mouse click on slider to jump to the clicked position
+slider.bind("<Button-1>", jump_to_position)
+entry_value.bind("<Return>", entry_useful)  # Bind the "Enter" key to the entry box
+
+
+# Being a cringe simp
+
+master.mainloop()
